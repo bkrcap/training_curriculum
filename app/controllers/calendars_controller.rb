43d1875accfa43bbc0dc7bd90c9b192555ae7503
spiderweb0802 <81157779+bkrcap@users.noreply.github.com>
2021-04-09 19:30:51 +0900

    Revert "Merge pull request #9 from bkrcap/second_training"
    This reverts commit 603596935bd16a44f1652276ee6b64e6ce8d23b1, reversing
    changes made to 816414494b26e59dc4ccab83511d6cb7f6155be0.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,6 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -16,8 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    #Issue4。params.require(:calendars)をparams.require(:plan)に変更
-    params.require(:plan).permit(:date, :plan)
+    params.require(:calendars).permit(:date, :plan)
   end
 
   def get_week
@@ -36,21 +34,9 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-
-      wday_num = Date.today.wday + x # wdayメソッドを用いて取得した数値。timesメソッドのブロック変数は繰り返される毎に１ずつ増えていく。と言うことは、Xが勝手に1増えていくと言うこと。
-      if wday_num >= 7#「wday_numが7以上の場合」という条件式
-        wday_num = wday_num -7
-      end
-
-      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[wday_num]}#wdaysから値を取り出す記述。wdays[wday_num(ここは数字であるべき。成立するのはwdayメソッドが配列を整数で抜く特性があって勝手に数字になってるから。)]
-
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
 
   end
 end
-
-
-
-
-  
